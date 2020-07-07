b98bb2eba347ffa447938febaa49da0c6117038f
traimuathu <do.minh.tung.2108@gmail.com>
2020-07-07 19:08:47 +0700

    config express, ejs...

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ module.exports = app => {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended: false}));
 
-	app.use('static', express.static(path.join(__dirname, 'public')));
+	app.use(express.static(path.join(__dirname, 'public')));
 
 	app.set('view engine', 'ejs');
 	app.set('views', path.join(__dirname, 'views'));
