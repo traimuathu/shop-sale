df0883a49973d5e97c726ee90f016e913b449418
traimuathu <do.minh.tung.2108@gmail.com>
2020-07-09 20:55:06 +0700

    fix session

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
+const session = require('express-session');
 
 module.exports = app => {
 	require('dotenv').config();
+
+	// Import Mongo
+	require('./configs/db.config')();
 
 	app.use(morgan('dev'));
 	app.use(favicon(path.join(__dirname, 'public', 'images', 'ic.ico')));
@@ -14,11 +18,18 @@ module.exports = app => {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended: false}));
 
+	app.use(session({
+		secret: process.env.SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {maxAge: 300000}
+	}));
+
 	app.use(express.static(path.join(__dirname, 'public')));
 
 	app.set('view engine', 'ejs');
 	app.set('views', path.join(__dirname, 'views'));
-	
+
 	// Router
 	app.use(require('./routes/auth.route'));
 
